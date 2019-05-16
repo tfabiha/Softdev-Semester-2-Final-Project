97ea0f3e99472838f107d67e953f75b890f81fb4
rachel-ng <rng@stuy.edu>
2019-05-16 17:57:52 -0400

    nooooooooooooo

--- util/db.py
+++ util/db.py
@@ -4,9 +4,10 @@ if os.environ['PWD'] == '/var/www/ccereal/ccereal':
     DIR = os.path.dirname(__file__) or '.'
     DIR += '/'
 else: DIR = ""
+DB_FILE = DIR + "data/database.db"
 
 def create_table():
-    db = sqlite3.connect(DIR + "data/database.db")
+    db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     command = "CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, wins INT, losses INT)"
     c.execute(command)
