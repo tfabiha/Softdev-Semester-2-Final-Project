fea98db76ac9baadfc78d2fae2d434113ee6f2b6
Imad <ibelkebir@stuy.edu>
2019-05-20 13:00:25 -0400

    comments

--- static/sockets.js
+++ static/sockets.js
@@ -1,6 +1,13 @@
+'''
+connect to server with socket
+'''
 var socket = io.connect('http://' + document.domain + ':' + location.port);
+
 var msgbox = document.getElementById('msg');
 
+'''
+when message is received from server
+'''
 socket.on('message', function(msg) {
   var chat = document.getElementById('chat');
   var newMsg = document.createElement('li');
@@ -12,12 +19,18 @@ socket.on('message', function(msg) {
   chat.appendChild(newMsg);
 });
 
+'''
+send message to server, calls message function
+'''
 var sendMessage = function() {
   var newMsg = msgbox.value;
   socket.send(newMsg);
   msgbox.value = "";
 }
 
+'''
+Sends request to server to call joinRoom with argument lobby so client joins room called lobby
+'''
 var joinRoom = function() {
   socket.emit('joinRoom', 'lobby');
 };
